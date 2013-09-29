c4e8fa81269642352e99fbb4260b002def5cf3c7
HR Admin <cameron@hackreactor.com>
2013-09-28 19:07:26 -0700

    silence output from pushd/popd in .bin/link.sh (salt is paranoid)

--- .bin/link.sh
+++ .bin/link.sh
@@ -11,11 +11,11 @@ script_dir="$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )"
 dots_dir=$script_dir/../
 
 
-pushd $dots_dir
+pushd $dots_dir >/dev/null
 for pkg in $pkgs; do
     for f in `ls -A $pkg`; do
 	rm -r $HOME/$f 2> /dev/null
     done
     stow $pkg
 done
-popd
+popd >/dev/null
