c5d0769f0d305cab591f7acbebda95af703a377b
HR Admin <cameron@hackreactor.com>
2013-09-28 19:31:01 -0700

    dont exit on error in when clearing the way in link.sh

--- .bin/link.sh
+++ .bin/link.sh
@@ -6,16 +6,17 @@ set -e
 # (folders in .dotfiles whose contents should be linked into ~/)
 pkgs="bash git etc editors"
 
-
 script_dir="$( cd $( dirname ${BASH_SOURCE[0]} ) && pwd )"
-dots_dir=$script_dir/../
-
+dots_dir=$script_dir/..
+home_dir=$dots_dir/..
 
 pushd $dots_dir >/dev/null
 for pkg in $pkgs; do
+    set +e
     for f in `ls -A $pkg`; do
-	rm -r $HOME/$f 2> /dev/null
+        rm -r $home_dir/$f 2>/dev/null 
     done
+    set -e
     stow $pkg
 done
 popd >/dev/null
